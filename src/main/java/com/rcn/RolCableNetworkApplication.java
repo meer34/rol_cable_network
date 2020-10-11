54c3531e76c1ff1c0c330653050eb7fc589abda6
Meer <Meer@SkyLARK>
2020-10-11 12:20:56 +0530

    First Commit

---
+++ src/main/java/com/rcn/RolCableNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.rcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolCableNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolCableNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rcn/RolCableNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolCableNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
